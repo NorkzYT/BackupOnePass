66a6175409ece95487460616a028a613ae973197
Norkz <richardlora557@gmail.com>
2025-03-22 00:15:30 -0400

    ✨ (env): add COMPOSE_BAKE variable to .env.example 🔧 (husky): comment out commitlint in commit-msg hook 🔄 (install_1password): specify version for 1Password download script
    Add the `COMPOSE_BAKE` variable to the `.env.example` file to provide a new configuration option. Comment out the commitlint command in the Husky commit-msg hook to prevent it from running. Update the 1Password installation script to specify the version being downloaded, ensuring compatibility and clarity in the installation process.

--- docker/config/install_1password.sh
+++ docker/config/install_1password.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-# Detect architecture and download the appropriate version of 1Password
+# Specific download of 1Password version links:
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/misc/1password-gui/sources.json
+
+# Detect architecture and download the appropriate version of 1Password (Stable)
 ARCH=$(dpkg --print-architecture)
 if [ "$ARCH" = "amd64" ]; then
-    curl -sSO https://downloads.1password.com/linux/tar/stable/x86_64/1password-latest.tar.gz
+    FILE="1password-8.10.56.x64.tar.gz"
+    curl -sSO https://downloads.1password.com/linux/tar/stable/x86_64/$FILE
 elif [ "$ARCH" = "arm64" ]; then
-    curl -sSO https://downloads.1password.com/linux/tar/stable/aarch64/1password-latest.tar.gz
+    FILE="1password-8.10.56.arm64.tar.gz"
+    curl -sSO https://downloads.1password.com/linux/tar/stable/aarch64/$FILE
 else
     echo "Unsupported architecture"
     exit 1
 fi
 
 # Extract and move the files
-tar -xf 1password-latest.tar.gz
+tar -xf "$FILE"
 mkdir -p /opt/1Password
 mv 1password-*/* /opt/1Password
 /opt/1Password/after-install.sh
-rm 1password-latest.tar.gz
+rm "$FILE"
+
+# March 18 2025
+# 1Password version 8.10.68 (Currently unsupported)
+
+# December 12 2024
+# 1Password version 8.10.56 (Supported)
